0f5448eaf853b49d7988c87cc9253712434eec62
Jenny Wong <lists@jwong.co.uk>
2013-10-09 20:20:28 +0100

    Extracting Flight details so this can be changed at any point.

---
+++ interestedFlights.php
@@ -0,0 +1,32 @@
+/*
+ *  This data should be arrays within arrays or an array with bands -> properties
+ */
+$thursday = array(
+	'EZY1832' 	=> 'Sigur Ros',
+	'KL1093'	=> 'New Order'
+);
+
+$friday = array(
+	'SN2173' => 'Rage Against the Machine',
+	'EZY1832' => 'Regurgitator',
+	'KL1081' => 'Norah Jones',
+	'DL064' => 'Third Eye Blind',
+	'EZY1898' => 'VV Brown',
+	'KL1083' => 'OK Go',
+	'AF2198' => 'Jack Johnson'
+);
+
+$sunday = array(
+	'KL1074' => 'Tool',
+	'KL1088' => 'Hugh Laurie',
+	'SN2178' => 'Michael Buble',
+	'BE7217' => 'RHCP',
+	'KL1094' => 'Jason Mraz',
+	'EZY1835' => 'Bob Dylan',
+	'AF1169' => 'Bruno Mars'
+);
+
+$monday = array(
+	'SN2174' => 'Emeli Sande',
+	'DL065' => 'Example'
+);
